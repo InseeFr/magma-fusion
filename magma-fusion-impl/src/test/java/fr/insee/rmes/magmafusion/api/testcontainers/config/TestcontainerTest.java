e016b1ee451be4fd451cb21c127356a8eccfd605
GtanSndil <mail@ad.insee.intra>
2026-09-01 15:58:59 +0200

    refactor: onoy one class TestcontainerTest

---
+++ magma-fusion-impl/src/test/java/fr/insee/rmes/magmafusion/api/testcontainers/config/TestcontainerTest.java
@@ -0,0 +1,27 @@
+package fr.insee.rmes.magmafusion.api.testcontainers.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeAll;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.context.TestPropertySource;
+
+@Slf4j
+@TestPropertySource(properties = "spring.profiles.active=security.disabled")
+public class TestcontainerTest {
+    static GraphDBContainer container = new GraphDBContainer("ontotext/graphdb:10.8.8").withReuse(false);
+
+    @BeforeAll
+    static void startContainer(){
+        container.start();
+
+    }
+
+
+    @DynamicPropertySource
+    static void overrideSpringProperties(DynamicPropertyRegistry registry) {
+        String url  = "http://" + container.getHost() + ":" + container.getMappedPort(7200)+ "/repositories/magmafusion";
+        log.info("Graphdb URL: " + url);
+        registry.add("fr.insee.rmes.magmafusion.api.sparqlEndpoint", () -> url) ;
+    }
+}
