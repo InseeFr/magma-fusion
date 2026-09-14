a42d1b3db3692eca81ad6caefe0963d7dd84bf5c
GtanSndil <mail@ad.insee.intra>
2026-09-14 14:46:39 +0200

    test: style

--- magma-fusion-impl/src/test/java/fr/insee/rmes/magmafusion/api/testcontainers/OperationsQueriesTest.java
+++ magma-fusion-impl/src/test/java/fr/insee/rmes/magmafusion/api/testcontainers/OperationsQueriesTest.java
@@ -2,10 +2,7 @@ package fr.insee.rmes.magmafusion.api.testcontainers;
 
 import fr.insee.rmes.magmafusion.api.OperationsEndpoints;
 import fr.insee.rmes.magmafusion.api.testcontainers.config.TestContainer;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -33,48 +30,49 @@ class OperationsQueriesTest extends TestContainer {
     @Autowired
     private ObjectMapper objectMapper;
 
-    // =========================================================
-    //    /operations/rapportQualite/{id}
-    // =========================================================
+    @Nested
+    @DisplayName("operations/rapportQualite/{id}")
+    class GetOperation {
 
-    @Test
-    @DisplayName("When getRapportQualiteByCode, returns rapport with all rubrique types (TEXT, DATE, RICH_TEXT, GEOGRAPHY, ORGANIZATION, CODE_LIST)")
-    void should_return_rapportQualite_with_all_rubrique_types() throws Exception {
-        var response = endpoints.getRapportQualiteByCode("9999");
-        var result = response.getBody();
+        @Test
+        @DisplayName("When getRapportQualiteByCode, returns rapport with all rubrique types (TEXT, DATE, RICH_TEXT, GEOGRAPHY, ORGANIZATION, CODE_LIST)")
+        void should_return_rapportQualite_with_all_rubrique_types() throws Exception {
+            var response = endpoints.getRapportQualiteByCode("9999");
+            var result = response.getBody();
 
-        assertNotNull(result);
-        String data = objectMapper.writeValueAsString(result);
-        String expected = new String(
-                Objects.requireNonNull(getClass().getClassLoader()
-                                .getResourceAsStream("testcontainers/rapportQualite-9999-expected.json"))
-                        .readAllBytes(),
-                StandardCharsets.UTF_8
-        );
-        JSONAssert.assertEquals(expected, data, false);
-    }
+            assertNotNull(result);
+            String data = objectMapper.writeValueAsString(result);
+            String expected = new String(
+                    Objects.requireNonNull(getClass().getClassLoader()
+                                    .getResourceAsStream("testcontainers/rapportQualite-9999-expected.json"))
+                            .readAllBytes(),
+                    StandardCharsets.UTF_8
+            );
+            JSONAssert.assertEquals(expected, data, false);
+        }
 
-    @Test
-    @DisplayName("When getRapportQualiteByCode for rapport without rubriques, returns rapport without rubriques")
-    void should_return_rapportQualite_without_rubriques() throws Exception {
-        var response = endpoints.getRapportQualiteByCode("9998");
-        var result = response.getBody();
+        @Test
+        @DisplayName("When getRapportQualiteByCode for rapport without rubriques, returns rapport without rubriques")
+        void should_return_rapportQualite_without_rubriques() throws Exception {
+            var response = endpoints.getRapportQualiteByCode("9998");
+            var result = response.getBody();
 
-        assertNotNull(result);
-        String data = objectMapper.writeValueAsString(result);
-        String expected = new String(
-                Objects.requireNonNull(getClass().getClassLoader()
-                                .getResourceAsStream("testcontainers/rapportQualite-9998-expected.json"))
-                        .readAllBytes(),
-                StandardCharsets.UTF_8
-        );
-        JSONAssert.assertEquals(expected, data, false);
-    }
+            assertNotNull(result);
+            String data = objectMapper.writeValueAsString(result);
+            String expected = new String(
+                    Objects.requireNonNull(getClass().getClassLoader()
+                                    .getResourceAsStream("testcontainers/rapportQualite-9998-expected.json"))
+                            .readAllBytes(),
+                    StandardCharsets.UTF_8
+            );
+            JSONAssert.assertEquals(expected, data, false);
+        }
 
-    @Test
-    @DisplayName("When getRapportQualiteByCode with unknown id, returns 404")
-    void should_return_404_when_rapportQualite_inexistant() throws Exception {
-        mockMvc.perform(get("/operations/rapportQualite/0000"))
-                .andExpect(status().isNotFound());
+        @Test
+        @DisplayName("When getRapportQualiteByCode with unknown id, returns 404")
+        void should_return_404_when_rapportQualite_inexistant() throws Exception {
+            mockMvc.perform(get("/operations/rapportQualite/0000"))
+                    .andExpect(status().isNotFound());
+        }
     }
 }
