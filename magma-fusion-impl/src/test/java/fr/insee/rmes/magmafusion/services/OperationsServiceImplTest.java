91a0dd93ade47a3be002860ee680fdcecc8398ba
GtanSndil <mail@ad.insee.intra>
2026-09-24 16:35:06 +0200

    refactor: rename test class

--- magma-fusion-impl/src/test/java/fr/insee/rmes/magmafusion/services/OperationsServiceImplTest.java
+++ magma-fusion-impl/src/test/java/fr/insee/rmes/magmafusion/services/OperationsServiceImplTest.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class SeriesOperationsServiceImplTest {
+class OperationsServiceImplTest {
 
     private OperationsServiceImpl service;
 
